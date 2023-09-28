8f9803c531a4e844b5254219b4a2cc201f419c41
Steven Enten <steven@enten.fr>
2023-09-28 13:53:10 +0200

    refactor(lib): better trace of console calls

--- projects/ngx-sticky/src/lib/sticky.directive.ts
+++ projects/ngx-sticky/src/lib/sticky.directive.ts
@@ -777,10 +777,11 @@ export class NgxStickyDirective extends NgxStickyBaseController implements After
 
     const nestedStickyError = new Error('Nested sticky is not support. Sticky will not work.');
 
-    const logLevel = isDevMode() ? 'error' : 'warn';
-    const logLevelLogger = console[logLevel];
-
-    logLevelLogger(nestedStickyError);
+    if (isDevMode()) {
+      console.error(nestedStickyError);
+    } else {
+      console.warn(nestedStickyError);
+    }
 
     return true;
   }
